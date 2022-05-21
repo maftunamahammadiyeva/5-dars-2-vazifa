8b1cac48491398b866f7e2454f386b22a710e28e
maftunamahammadiyeva <mahammadiyevamaftuna01@gmail.com>
2022-05-22 00:25:57 +0500

    added

--- js/index.js
+++ js/index.js
@@ -4,12 +4,12 @@ const johnMass = Number(prompt("Johnning vazningizni kiriting"));
 const johnHeight = Number(prompt("Johnning bo'yingizni kiriting"));
 const markHeightmeter= (markHeight/100);
 const johnHeightmeter= (johnHeight/100);
-const markBmi = (markMass/(markHeightmeter**2));
-const johnBmi = (johnMass/(johnHeightmeter**2));
+const markBmi = (Math.floor(markMass/(markHeightmeter**2)));
+const johnBmi = (Math.floor(johnMass/(johnHeightmeter**2)));
 console.log(markBmi);
 console.log(johnBmi);
 if (markBmi>=johnBmi) {
-console.log("Markning BMI katta");
+console.log(`Markning ${markBmi} bu katta Johnikidan, uniki ${johnBmi}`);
 } else{
-  console.log("Johnning BMI katta");
+  console.log(`Johnning ${johnBmi} bu katta Marknikidan, uniki ${markBmi}`);
 }
